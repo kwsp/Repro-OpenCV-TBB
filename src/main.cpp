aeb8b51052f86e69e883eef0102283e1f4f3a78f
tnie <tnie@tuta.io>
2024-07-11 01:00:53 -0500

    print OpenCV build information

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <fmt/core.h>
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
 int main(int argc, char *argv[]) {
   fmt::print("Hello, world!\n");
+  std::cout << cv::getBuildInformation();
   return 0;
 }
